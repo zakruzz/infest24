396dcaf8e6b240de43f4c535bcaea450f691eb86
zakruzz <therenopeople9@gmail.com>
2023-10-01 01:31:09 +0700

    Update Buat Hosting

--- resources/views/layouts/main/header.blade.php
+++ resources/views/layouts/main/header.blade.php
@@ -3,23 +3,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>INFEST 2024</title>
-    <link rel="stylesheet" href="{{ asset('assets/main/app/bootstrap/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/main/app/swiper/swiper-bundle.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/main/app/dist/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/main/aset/font/font-awesome.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/main/aset/font/risebot.css') }}" />
+    <link rel="stylesheet" href="{{ asset('public/assets/main/app/bootstrap/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets/main/app/swiper/swiper-bundle.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets/main/app/dist/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets/main/aset/font/font-awesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets/main/aset/font/risebot.css') }}" />
     <link href="//cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet">
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.js"></script>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <!-- Favicon and Touch Icons  -->
-    <link rel="shortcut icon" href="{{ asset('assets/main/images/favicon.png') }}">
-    <link rel="apple-touch-icon-precomposed" href="{{ asset('assets/main/images/favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('public/assets/main/images/favicon.png') }}">
+    <link rel="apple-touch-icon-precomposed" href="{{ asset('public/assets/main/images/favicon.png') }}">
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <style>
         @font-face {
             font-family: "resobot-bold";
-            src: url("{{ asset('assets/main/aset/font/GeneralSans-Bold.otf') }}");
+            src: url("{{ asset('public/assets/main/aset/font/GeneralSans-Bold.otf') }}");
             font-weight: normal;
             font-style: normal;
             font-display: block;
@@ -42,7 +42,7 @@
 
         .team-details .image {
             z-index: 999;
-            background: url("{{ asset('assets/images/backgroup/bg_img_team_4.png') }}") center center no-repeat;
+            background: url("{{ asset('public/assets/images/backgroup/bg_img_team_4.png') }}") center center no-repeat;
             width: 194px;
             height: 220px;
             min-width: 194px;
@@ -53,8 +53,8 @@
         .team-details .image img {
             width: 194px;
             height: 220px;
-            -webkit-mask-image: url("{{ asset('assets/images/backgroup/bg_img_team_4.png') }}");
-            mask-image: url("{{ asset('assets/images/backgroup/bg_img_team_4.png') }}");
+            -webkit-mask-image: url("{{ asset('public/assets/images/backgroup/bg_img_team_4.png') }}");
+            mask-image: url("{{ asset('public/assets/images/backgroup/bg_img_team_4.png') }}");
             -webkit-mask-position: center center;
             mask-position: center center;
             -webkit-mask-repeat: no-repeat;
@@ -133,13 +133,13 @@
 
         .icon-box .top {
             text-align: center;
-            -webkit-mask-image: url("{{ asset('assets/images/backgroup/bg_box_1.png') }}");
-            mask-image: url("{{ asset('assets/images/backgroup/bg_box_1.png') }}");
+            -webkit-mask-image: url("{{ asset('public/assets/images/backgroup/bg_box_1.png') }}");
+            mask-image: url("{{ asset('public/assets/images/backgroup/bg_box_1.png') }}");
             -webkit-mask-position: center center;
             mask-position: center center;
             -webkit-mask-repeat: no-repeat;
             mask-repeat: no-repeat;
-            background: url("{{ asset('assets/images/backgroup/bg_box_1.png') }}");
+            background: url("{{ asset('public/assets/images/backgroup/bg_box_1.png') }}");
             background-position: center, center;
             background-repeat: no-repeat;
             background-size: cover;
@@ -190,7 +190,7 @@
 
         .team-box .image {
             z-index: 999;
-            background: url("{{ asset('assets/images/backgroup/bg_img_team.png') }}") center center no-repeat;
+            background: url("{{ asset('public/assets/images/backgroup/bg_img_team.png') }}") center center no-repeat;
             width: 156px;
             height: 176px;
             margin: 0 auto;
@@ -200,8 +200,8 @@
         .team-box .image img {
             width: 156px;
             height: 176px;
-            -webkit-mask-image: url("{{ asset('assets/images/backgroup/bg_img_team.png') }}");
-            mask-image: url("{{ asset('assets/images/backgroup/bg_img_team.png') }}");
+            -webkit-mask-image: url("{{ asset('public/assets/images/backgroup/bg_img_team.png') }}");
+            mask-image: url("{{ asset('public/assets/images/backgroup/bg_img_team.png') }}");
             -webkit-mask-position: center center;
             mask-position: center center;
             -webkit-mask-repeat: no-repeat;
@@ -216,12 +216,12 @@
             margin-top: -102px;
             border-radius: 20px;
             position: relative;
-            -webkit-mask-image: url("{{ asset('assets/images/backgroup/bg_team.png') }}");
-            mask-image: url("{{ asset('assets/images/backgroup/bg_team.png') }}");
+            -webkit-mask-image: url("{{ asset('public/assets/images/backgroup/bg_team.png') }}");
+            mask-image: url("{{ asset('public/assets/images/backgroup/bg_team.png') }}");
             -webkit-mask-position: center center;
             mask-position: center center;
             -webkit-mask-repeat: no-repeat;
-            background: url("{{ asset('assets/images/backgroup/bg_team.png') }}");
+            background: url("{{ asset('public/assets/images/backgroup/bg_team.png') }}");
             background-position: center, center;
             background-size: cover;
             z-index: 99;
@@ -250,7 +250,7 @@
         }
 
         .project-info-form.form-login {
-            background-image: url("{{ asset('assets/images/backgroup/bg_login.png') }}");
+            background-image: url("{{ asset('public/assets/images/backgroup/bg_login.png') }}");
             background-color: transparent;
             border: none;
             position: relative;
@@ -275,7 +275,7 @@
         .project-info-form.form-login .title.link {
             margin-bottom: 0 !important;
             height: 57px;
-            background-image: url("{{ asset('assets/images/backgroup/bg_link_1.png') }}");
+            background-image: url("{{ asset('public/assets/images/backgroup/bg_link_1.png') }}");
             background-repeat: no-repeat;
             display: flex;
             align-items: center;
@@ -308,12 +308,12 @@
         }
 
         .project-info-form.form-login.style2 {
-            background-image: url("{{ asset('assets/images/backgroup/bg_login_2.png') }}");
+            background-image: url("{{ asset('public/assets/images/backgroup/bg_login_2.png') }}");
             padding: 20px 30px 35px;
         }
 
         .project-info-form.form-login.style2 .title.link {
-            background-image: url("{{ asset('assets/images/backgroup/bg_link_2.png') }}");
+            background-image: url("{{ asset('public/assets/images/backgroup/bg_link_2.png') }}");
             background-repeat: no-repeat;
             display: flex;
             align-items: center;
@@ -341,7 +341,7 @@
                     #09121d 3.92%,
                     rgba(9, 18, 29, 0) 36.79%,
                     #09121d 83.98%),
-                url("{{ asset('assets/images/backgroup/bg_pagetitle.png') }}"), center, center,
+                url("{{ asset('public/assets/images/backgroup/bg_pagetitle.png') }}"), center, center,
                 no-repeat;
             mix-blend-mode: luminosity;
             background-position: center, center;
@@ -350,7 +350,7 @@
             opacity: 0.9;
         }
 .project-info-form.form-login {
-    background-image: url("{{ asset('assets/images/backgroup/bg_login.png') }}");
+    background-image: url("{{ asset('public/assets/images/backgroup/bg_login.png') }}");
     background-color: transparent;
     border: none;
     position: relative;
@@ -371,7 +371,7 @@
 .project-info-form.form-login .title.link {
     margin-bottom: 0 !important;
     height: 57px;
-    background-image: url("{{ asset('assets/images/backgroup/bg_link_1.png') }}");
+    background-image: url("{{ asset('public/assets/images/backgroup/bg_link_1.png') }}");
     background-repeat: no-repeat;
     display: flex;
     align-items: center;
@@ -399,11 +399,11 @@
     color: #86ff00;
 }
 .project-info-form.form-login.style2 {
-    background-image: url("{{ asset('assets/images/backgroup/bg_login_2.png') }}");
+    background-image: url("{{ asset('public/assets/images/backgroup/bg_login_2.png') }}");
     padding: 20px 30px 35px;
 }
 .project-info-form.form-login.style2 .title.link {
-    background-image: url("{{ asset('assets/images/backgroup/bg_link_2.png') }}");
+    background-image: url("{{ asset('public/assets/images/backgroup/bg_link_2.png') }}");
     background-repeat: no-repeat;
     display: flex;
     align-items: center;
@@ -430,7 +430,7 @@
             rgba(9, 18, 29, 0) 36.79%,
             #09121d 83.98%
         ),
-        url("{{ asset('assets/images/backgroup/bg_pagetitle.png') }}"), center, center,
+        url("{{ asset('public/assets/images/backgroup/bg_pagetitle.png') }}"), center, center,
         no-repeat;
     mix-blend-mode: luminosity;
     background-position: center, center;
@@ -462,7 +462,7 @@
             <div class="container">
                 <div id="site-header-inner">
                     <div class="header__logo">
-                        <a href="/"><img src="{{ asset('assets/images/logo/logo@2x.png') }}" alt=""></a>
+                        <a href="/"><img src="{{ asset('public/assets/images/logo/logo2x.png') }}" alt=""></a>
                     </div>
                     <nav id="main-nav" class="main-nav">
                         <ul id="menu-primary-menu" class="menu">
